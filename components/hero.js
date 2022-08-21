c0bc386897a09b8d1346203a398fc9f0253e8e39
Sergio Delgado <sergiodelgado@Sergios-Mac-mini.local>
2022-08-21 17:34:37 -0500

    Feature: add hero

---
+++ components/hero.js
@@ -0,0 +1,15 @@
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero__container">
+                Hello, i am <span className="hero__highlight"> Sergio Delgado</span>.<br /> I am a frontend developer
+                <button className="button">
+                    View my work
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
